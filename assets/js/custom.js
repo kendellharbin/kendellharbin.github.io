f8521a465d3aa84b742c9626299c0d477347628d
Roger Link <roger.link@cerner.com>
2016-11-11 18:43:35 -0600

    added postit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-    var items = ['pin.png', 'pen.png', 'tape.png'];
+    var items = ['pin.png', 'pen.png', 'tape.png','postit.png'];
 
 
     items.forEach(function(item, index){
