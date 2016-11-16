1ad3f4bfe742a6d9baeaa22a4fe12ede7e233194
Roger Link <roger.link@cerner.com>
2016-11-15 19:33:49 -0600

    small refactor

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
 
     var items = ['pin.png', 'pen.png', 'tape2.png','postit.png', 'whistle.png'];
 
-
     items.forEach(function(item, index){
         var section = index + 2;
     	var item_loc = `./assets/imgs/home/${item}`;
         var item_id = `section${index}`;
         var item_src = `<a href="#" class="item" data-src=${section}><img src=${item_loc} /></a>`;
-    	$(item_src).appendTo("#section0 .content");
+    	// $(item_src).appendTo("#section0 .content");
+        $(item_src).appendTo("#fullpage:first-child");
 
     });
 
@@ -18,8 +18,8 @@ $(document).ready(function(){
     var wh = $(window).height();
 
     $(".item").each(function(i){
-	    var rotationNum=Math.round((Math.random()*360)+1);
-	    var rotation="rotate("+rotationNum+"deg)";
+	    var rotationNum = Math.round((Math.random()*360)+1);
+	    var rotation = "rotate("+rotationNum+"deg)";
 	    var posx = Math.round(Math.random() * ww)-40;
 	    var posy = Math.round(Math.random() * wh)-40;
 	    $(this).css("top", posy + "px").css("left", posx + "px").css("transform",rotation).css("-ms-transform",rotation).css("-webkit-transform",rotation);
